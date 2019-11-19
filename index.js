b3838efbb1dbe68d2f97db0da457aed58a2340a7
maxdevos49 <maxdevos49@gmail.com>
2019-11-19 17:23:03 -0600

    added template middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+var bodyParser = require('body-parser')
 
 const PORT = 8080 || process.env.PORT;
 
 //middleware
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(express.static("public"));
 
---
+++ middleware.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    return function (req, res, next) {
+        
+
+        
+
+      // Implement the middleware function based on the options object
+      next()
+    }
+  }
